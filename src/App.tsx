8b8223a5b885de4766e436317da520a14e1edaa6
bajormar <marius.bajorunas@gmail.com>
2020-02-07 22:24:33 +0200

    Include tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="flex flex-col w-3/4 mx-auto my-12 items-center">
+          <h1>Super cool page</h1>
+          <button onClick={() => console.log("I was clicked")}>
+              I am a button
+          </button>
+      </div>
   );
-}
+};
 
 export default App;
