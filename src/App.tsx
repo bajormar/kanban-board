380557dbc3085f33935daf37b94a00617ff4af4a
bajormar <marius.bajorunas@gmail.com>
2020-02-07 23:17:29 +0200

    Include eslint + prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 const App = () => {
   return (
-      <div className="flex flex-col w-3/4 mx-auto my-12 items-center">
-          <h1>Super cool page</h1>
-          <button onClick={() => console.log("I was clicked")}>
-              I am a button
-          </button>
-      </div>
+    <div className="flex flex-col w-3/4 mx-auto my-12 items-center">
+      <h1>Super cool page</h1>
+      <button onClick={() => console.log('I was clicked')}>I am a button</button>
+    </div>
   );
 };
 
